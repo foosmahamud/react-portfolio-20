50212c764258a1e8ba3a9b953ce2ddf9a41be39c
Foos Mahamud <foosmahamud55@gmail.com>
2020-12-07 22:33:25 -0600

    deleted extra code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
-import FavoritesList from "./pages/FavoritesList";
 
 function App() {
   return (
     <Router>
       <div>
-        <StoreProvider>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
-            <Route exact path="/favorites" component={FavoritesList} />
-            <Route exact path="/posts/:id" component={Detail} />
-            <Route component={NoMatch} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} />
           </Switch>
-        </StoreProvider>
       </div>
     </Router>
   );
