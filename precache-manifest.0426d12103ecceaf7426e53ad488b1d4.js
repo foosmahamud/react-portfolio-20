d89880c83b5e1ed3982a46b5751270d93a7a8107
Foos Mahamud <foosmahamud55@gmail.com>
2021-07-31 20:11:55 -0500

    updated to this one

---
+++ precache-manifest.0426d12103ecceaf7426e53ad488b1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721eb8981a604ed6c0da9b134a8337d8",
+    "url": "/react-portfolio-20/index.html"
+  },
+  {
+    "revision": "0980ff37c8c9dbc4e4c3",
+    "url": "/react-portfolio-20/static/js/2.3cfb2bbb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio-20/static/js/2.3cfb2bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb3fc92a6a2db3195d7",
+    "url": "/react-portfolio-20/static/js/main.6f9a74f4.chunk.js"
+  },
+  {
+    "revision": "d5709db0e10eb0a1d47d",
+    "url": "/react-portfolio-20/static/js/runtime-main.1542e276.js"
+  }
+]);
